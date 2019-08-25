9d0c70e1b3aef7d6d43d2be195a755d3e22f3c64
a16jenja <49199287+a16jenja@users.noreply.github.com>
2019-08-26 00:52:19 +0200

    Updates

---
+++ precache-manifest.f1c7da2aff51b836917d45a3ef94053d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79dc39509d3f3d4fb9843fde304cbc5",
+    "url": "/RPS-Game/index.html"
+  },
+  {
+    "revision": "72e94fe2325957b24355",
+    "url": "/RPS-Game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5b6c53b088f3465ebba3",
+    "url": "/RPS-Game/static/css/main.cf39c8fa.chunk.css"
+  },
+  {
+    "revision": "72e94fe2325957b24355",
+    "url": "/RPS-Game/static/js/2.39f1fb89.chunk.js"
+  },
+  {
+    "revision": "5b6c53b088f3465ebba3",
+    "url": "/RPS-Game/static/js/main.312104e1.chunk.js"
+  },
+  {
+    "revision": "1a7d76301152f735b872",
+    "url": "/RPS-Game/static/js/runtime~main.ded56ae9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RPS-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
